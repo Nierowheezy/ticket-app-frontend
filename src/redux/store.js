bea8723a628d44bd0b74304a62c0204aa5268a6e
Nierowheezy <olaniyiolabode99@yahoo.com>
2022-01-13 02:39:02 +0100

    firt commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
-
-import rootReducers from './reducers';
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import rootReducers from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
 
 export const store = createStore(
-    rootReducers,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
+  rootReducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export const persistor = persistStore(store);
 
-export default { store, persistor };
-
-
+// export default { store, persistor };
